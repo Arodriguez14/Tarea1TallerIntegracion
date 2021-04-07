7dc288ff4a89ea297172e8a9484c17ea63dbd2f2
Andrés Rodríguez <arodriguez14@uc.cl>
2021-04-07 14:50:23 -0300

    detalles

--- polls/views.py
+++ polls/views.py
@@ -36,34 +36,39 @@ def episodios(request, serie, num_temporada):
 
 def detalles_episodio(request, episode_id):
     r = requests.get('https://tarea-1-breaking-bad.herokuapp.com/api/episodes/'+str(episode_id))
-    title = r.json()[0]["title"]
-    season = r.json()[0]["season"]
-    characters = r.json()[0]["characters"]
-    episode = r.json()[0]["episode"]
-    air_date = r.json()[0]["air_date"]
-    series = r.json()[0]["series"]
-    
-    return render(request, 'polls/detalles_episodio.html', {"title": title, "season": season, "episode": episode, "air_date": air_date, "series":series, "episode_id": episode_id, "characters": characters})
+    try:
+        title = r.json()[0]["title"]
+        season = r.json()[0]["season"]
+        characters = r.json()[0]["characters"]
+        episode = r.json()[0]["episode"]
+        air_date = r.json()[0]["air_date"]
+        series = r.json()[0]["series"]
+        
+        return render(request, 'polls/detalles_episodio.html', {"title": title, "season": season, "episode": episode, "air_date": air_date, "series":series, "episode_id": episode_id, "characters": characters})
+    except IndexError:
+        return HttpResponse("Este episodio no existe")
 
 def personajes(request, personaje):
-    print(personaje)
-    r = requests.get('https://tarea-1-breaking-bad.herokuapp.com/api/characters?name='+str(personaje))
-    id_p = r.json()[0]["char_id"]
-    occupation = r.json()[0]["occupation"]
-    img = r.json()[0]["img"]
-    status = r.json()[0]["status"]
-    nickname = r.json()[0]["nickname"]
-    appearance = r.json()[0]["appearance"]
-    better_call_saul_appearance = r.json()[0]["better_call_saul_appearance"]
-    portrayed = r.json()[0]["portrayed"]
-    category = r.json()[0]["category"]
-    r2 = requests.get('https://tarea-1-breaking-bad.herokuapp.com/api/quote?author='+str(personaje))
-    quotes = [] 
-    
-    for elem in r2.json():
-        quotes.append([elem["quote_id"],elem["quote"]])
+    try: 
+        r = requests.get('https://tarea-1-breaking-bad.herokuapp.com/api/characters?name='+str(personaje))
+        id_p = r.json()[0]["char_id"]
+        occupation = r.json()[0]["occupation"]
+        img = r.json()[0]["img"]
+        status = r.json()[0]["status"]
+        nickname = r.json()[0]["nickname"]
+        appearance = r.json()[0]["appearance"]
+        better_call_saul_appearance = r.json()[0]["better_call_saul_appearance"]
+        portrayed = r.json()[0]["portrayed"]
+        category = r.json()[0]["category"]
+        r2 = requests.get('https://tarea-1-breaking-bad.herokuapp.com/api/quote?author='+str(personaje))
+        quotes = [] 
         
-    return render(request, 'polls/personaje.html', {"id": id_p, "occupation": occupation, "img": img, "status": status, "nickname":nickname, "appearance": appearance, "better_call_saul_appearance": better_call_saul_appearance, "portrayed": portrayed, "category": category, "name": personaje, "quotes": quotes})
+        for elem in r2.json():
+            quotes.append([elem["quote_id"],elem["quote"]])
+            
+        return render(request, 'polls/personaje.html', {"id": id_p, "occupation": occupation, "img": img, "status": status, "nickname":nickname, "appearance": appearance, "better_call_saul_appearance": better_call_saul_appearance, "portrayed": portrayed, "category": category, "name": personaje, "quotes": quotes})
+    except IndexError:
+        return HttpResponse("Este personaje no existe")
 
 def personajes2(request):
     searchWord = request.POST.get('search','')
